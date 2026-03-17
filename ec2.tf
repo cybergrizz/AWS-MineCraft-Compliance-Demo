b4fb7b88ca73e32fa131e3c77af86e4e5ea1568d
Kevin Douglas <121463119+cybergrizz@users.noreply.github.com>
2026-03-17 00:37:30 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,23 +1,7 @@
-data "aws_ami" "amazon_linux_2" {
-  most_recent = true
-  owners      = ["amazon"] # Specifies the official Amazon owner alias
-
-  filter {
-    name = "name"
-    # This pattern matches standard Amazon Linux 2 HVM AMIs
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-}
-
 resource "aws_instance" "minecrafte_server" {
-  ami                  = data.aws_ami.amazon_linux_2.id
+  ami                  = var.ami_id
   instance_type        = var.instance_type
-  subnet_id            = aws_subnet.public.id
+  subnet_id            = var.public_subnet
   iam_instance_profile = var.instance_profile
 
   tags = {
@@ -28,9 +12,9 @@ resource "aws_instance" "minecrafte_server" {
 }
 
 resource "aws_instance" "bastion" {
-  ami                  = data.aws_ami.amazon_linux_2.id
+  ami                  = var.ami_id
   instance_type        = var.instance_type
-  subnet_id            = aws_subnet.private.id
+  subnet_id            = var.private_subnet
   iam_instance_profile = var.instance_profile
 
   tags = {
