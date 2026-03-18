113f7eb6bf35211c9e9acb47d3258b459106898f
Kevin Douglas <121463119+cybergrizz@users.noreply.github.com>
2026-03-18 01:50:14 -0400

    Delete guardduty.tf

--- guardduty.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_guardduty_detector" "minecraft_detector" {
-  enable = true
-}
-
-resource "aws_guardduty_detector_feature" "s3_protection" {
-  detector_id = aws_guardduty_detector.minecraft_detector.id
-  name        = "S3_DATA_EVENTS"
-  status      = "ENABLED"
-}
