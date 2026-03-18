e8ff3d308f205540465f40ea3a5705f4b641b732
Kevin Douglas <121463119+cybergrizz@users.noreply.github.com>
2026-03-18 00:11:54 -0400

    Updating files to remove data.id var

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,9 @@
 resource "aws_instance" "minecrafte_server" {
-  ami                  = data.aws_ami.amazon_linux_2.id
-  instance_type        = var.instance_type
-  subnet_id            = aws_subnet.public.id
-  iam_instance_profile = var.instance_profile
+  ami                    = data.aws_ami.amazon_linux_2.id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public.id
+  iam_instance_profile   = aws_iam_instance_profile.ssm_profile.name
+  vpc_security_group_ids = [aws_security_group.minecraft_serversg.id]
 
   tags = {
     Name    = "minecraft-server"
@@ -12,10 +13,11 @@ resource "aws_instance" "minecrafte_server" {
 }
 
 resource "aws_instance" "bastion" {
-  ami                  = data.aws_ami.amazon_linux_2.id
-  instance_type        = var.instance_type
-  subnet_id            = aws_subnet.private.id
-  iam_instance_profile = var.instance_profile
+  ami                    = data.aws_ami.amazon_linux_2.id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.private.id
+  iam_instance_profile   = aws_iam_instance_profile.ssm_profile.name
+  vpc_security_group_ids = [aws_security_group.bastion.id]
 
   tags = {
     Name    = "bastion"
